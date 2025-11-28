b28fa1a7451b9fcb223ab14de243083c45969abf
jfernandecl <javierfernandez0987@gmail.com>
2025-11-28 14:46:29 +0100

    feat: initialize frontend with React, Vite, TypeScript, and Tailwind including basic setup, Tailwind/PostCSS config, main App, global CSS, and assets

---
+++ README.md
@@ -0,0 +1,1 @@
+## Proyecto QRmenu para restaurantes
---
+++ frontend/postcss.config.cjs
@@ -0,0 +1,7 @@
+// postcss.config.js (Corregido)
+module.exports = {
+  plugins: {
+    tailwindcss: {}, // <-- Usa 'tailwindcss'
+    autoprefixer: {},
+  },
+};
